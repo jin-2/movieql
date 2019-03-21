44a2a5597741ad06c24fec390e38efb9ab012d53
이수진 <jmakeaz@gmail.com>
2019-03-21 10:20:57 +0900

    Creating first mutation(영화 추가하기)

--- graphql/db.js
+++ graphql/db.js
@@ -38,4 +38,14 @@ export const deleteMovie = id => {
     }
 }
 
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
+}
+
 export default movies;
